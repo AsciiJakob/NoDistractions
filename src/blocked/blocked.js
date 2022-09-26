1eac038966d8b7a66717d495aa8f26131ccfb738
jakobjakobjakob2 <jakobjakobjakob2@gmail.com>
2022-09-26 11:08:32 +0200

    ui changes

--- src/blocked/blocked.js
+++ src/blocked/blocked.js
@@ -1,26 +1,27 @@
 var urlParams = new URLSearchParams(window.location.search);
 document.title = "Blocked | "+urlParams.get("url");
 
-document.querySelector("#displayURL").innerText = urlParams.get("url");
-document.querySelector("#displayURL").href = urlParams.get("url");
-document.querySelector("#nevermind").addEventListener("click", () => {
+document.getElementById("displayURL").innerText = urlParams.get("url");
+document.getElementById("displayURL").href = urlParams.get("url");
+document.getElementById("nevermind").addEventListener("click", () => {
 	browser.tabs.getCurrent().then(tab => {
 		console.log(tab);
 		browser.tabs.remove(tab.id);
 	});
 });
-document.querySelector("#disableND").addEventListener("click", () => {
+document.getElementById("disableND").addEventListener("click", () => {
 	browser.runtime.sendMessage({type: "setStatus", enabled: false}).then(res => {
 		browser.tabs.getCurrent().then(tab => {
 			browser.tabs.update(tab.response, { url: urlParams.get("url")}); 
 		});
 	});
 });
-document.querySelector("#visitAnyways").addEventListener("click", async () => {
+document.getElementById("visitAnyways").addEventListener("click", async () => {
 	const settings = (await browser.storage.local.get("settings")).settings;
 	const allowedLength = settings.visitAnywaysLength*60000;
 	browser.runtime.sendMessage({type: "addBlockingException", data: {tabId: (await browser.tabs.getCurrent()).id, allowedLength: allowedLength} }).then(res => {
-		document.querySelector("#blockedText").innerText = "Redirecting...";
+		document.getElementById("blockedText").innerText = "Redirecting...";
 		window.location = urlParams.get("url");
 	});
 });
+
