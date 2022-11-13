076ed9cc816b22986f053c1c2ddc6c82216a6b75
jakobjakobjakob2 <jakobjakobjakob2@gmail.com>
2022-11-13 15:59:10 +0100

    use lower resolution icon for notifications

--- src/background/Utilities.js
+++ src/background/Utilities.js
@@ -2,7 +2,7 @@ export default {
 	createNotification(name, alertmessage) {
 		browser.notifications.create(name, {
 			type: "basic",
-			iconUrl: "/static/assets/icon-medium.png",
+			iconUrl: "/static/assets/icon-low.png",
 			title: "NoDistractions",
 			message: alertmessage
 		});
