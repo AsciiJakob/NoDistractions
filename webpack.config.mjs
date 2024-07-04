b52183855a17ed94178b0d83cde748e42d8e1c10
Jakob <jakobjakobjakob2@gmail.com>
2024-07-04 23:56:55 +0200

    Fix indent

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -26,32 +26,32 @@ const config = {
     },
     plugins: [
         new CopyPlugin({
-        patterns: [
-            {
-                from: "src/static",
-                to: "static",
-                globOptions: {
-                    dot: false,
-                },
-            }, 
-            {
-                from: "src/manifest.json",
-                to: "manifest.json",
-                transform(content, absoluteFrom) {
-                    content = JSON.parse(content);
-                    content.version = pckgJson.version;
-                    content.description = pckgJson.description;
-                    return JSON.stringify(content, null, 2);
+            patterns: [
+                {
+                    from: "src/static",
+                    to: "static",
+                    globOptions: {
+                        dot: false,
+                    },
                 }, 
-            },
-            { from: "src/popup/Popup.html", to: "popup/Popup.html" },
-            { from: "src/popup/Popup.css", to: "popup/Popup.css" },
-            { from: "src/blocked/Blocked.html", to: "blocked/Blocked.html"},
-            { from: "src/blocked/Blocked.css", to: "blocked/Blocked.css"},
-            { from: "src/background/Background.html", to: "background/Background.html" },
-            { from: "src/settings/Settings.html", to: "settings/Settings.html" }, 
-            { from: "node_modules/webextension-polyfill/dist/browser-polyfill.js" }
-        ],
+                {
+                    from: "src/manifest.json",
+                    to: "manifest.json",
+                    transform(content, absoluteFrom) {
+                        content = JSON.parse(content);
+                        content.version = pckgJson.version;
+                        content.description = pckgJson.description;
+                        return JSON.stringify(content, null, 2);
+                    }, 
+                },
+                { from: "src/popup/Popup.html", to: "popup/Popup.html" },
+                { from: "src/popup/Popup.css", to: "popup/Popup.css" },
+                { from: "src/blocked/Blocked.html", to: "blocked/Blocked.html"},
+                { from: "src/blocked/Blocked.css", to: "blocked/Blocked.css"},
+                { from: "src/background/Background.html", to: "background/Background.html" },
+                { from: "src/settings/Settings.html", to: "settings/Settings.html" }, 
+                { from: "node_modules/webextension-polyfill/dist/browser-polyfill.js" }
+            ],
         }),
         new WebExtPlugin({
             sourceDir: __dirname+"/dist",
