ba70b3363d3e25556d1a3f7a7d45fa60787fcd43
jakobjakobjakob2 <jakobjakobjakob2@gmail.com>
2024-07-08 18:54:51 +0200

    remove commented out code & fix invalid function call

--- src/background/BlockHandler.js
+++ src/background/BlockHandler.js
@@ -54,36 +54,6 @@ export default {
         }
         return URLArr;
     },
-    // async getBlocklistURLPatterns() { // return blocklist in URL match patterns format, used by onBeforeRequest
-    //     let loadedBlocklist = await browser.storage.local.get("blockedSites_V1");
-    //     let URLPatterns = [];
-    //     if (Object.keys(loadedBlocklist) == 0) {
-    //         console.error("Failed to load the blocklist:", loadedBlocklist);
-    //     } 
-    //     for (const siteDomain of loadedBlocklist.blockedSites_V1) {
-    //         URLPatterns.push(toPattern(siteDomain));
-    //         if (!siteDomain.startsWith("*.") && !siteDomain.startsWith("www.") && !siteDomain.startsWith("!")) { // this is done for user friendliness sakes. I hope it's something sensical to do and doesn't cause any issues.
-    //             URLPatterns.push(toPattern("www."+siteDomain));
-    //         }
-    //     }
-    //     return URLPatterns;
-    // },
-    // async getBlocklistURLFilterArray() { // UrlFilter format, used by onHistoryStateUpdated
-    //     let loadedBlocklist = await browser.storage.local.get("blockedSites_V1");
-    //     let URLFilter = [];
-    //     if (Object.keys(loadedBlocklist) == 0) {
-    //         console.error("Failed to load the blocklist:", loadedBlocklist);
-    //     } 
-    //     for (const siteDomain of loadedBlocklist.blockedSites_V1) {
-    //         console.log("running patternToRegex on: ", toPattern(siteDomain));
-    //         URLFilter.push({urlMatches: patternToRegex(toPattern(siteDomain)).source});
-    //         if (!siteDomain.startsWith("*.") && !siteDomain.startsWith("www.") && !siteDomain.startsWith("!")) { // this is done for user friendliness sakes. I hope it's something sensical to do and doesn't cause any issues.
-    //             URLFilter.push({urlMatches: patternToRegex(toPattern("www."+siteDomain)).source});
-    //         }
-    //     }
-    //     console.log("using filter:", URLFilter);
-    //     return URLFilter;
-    // },
     async updateRequestListener() {
         await browser.webRequest.onBeforeRequest.removeListener(this.handleSite);
         await browser.webRequest.onBeforeRequest.addListener(this.handleSite, {urls: await this.getBlocklistParsed(), types: ["main_frame", "sub_frame"]}, ["blocking"]); // TODO: web_manifest type is not available on chrome, but is on firefox
@@ -91,7 +61,7 @@ export default {
         await browser.webNavigation.onHistoryStateUpdated.addListener(this.handleHistoryStateUpdate, {url: await this.getBlocklistParsed(true)});
     },
     async testAgainstBlocklist(url) { // In some cases we have to test against the blacklist instead of firefox doing it
-        const regexExpression = patternToRegex(...await this.getBlocklistURLPatterns());
+        const regexExpression = patternToRegex(...await this.getBlocklistParsed());
         console.log(regexExpression);
         return regexExpression.test(url);
     },
